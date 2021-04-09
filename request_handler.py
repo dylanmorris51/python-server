830c63bc2d2c85665aa787eeb576fab414b616ae
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-09 10:53:42 -0500

    create_customer

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location, create_location
-from customers import get_all_customers, get_single_customer
-from employees import get_all_employees, get_single_employee
+from customers import get_all_customers, get_single_customer, create_customer
+from employees import get_all_employees, get_single_employee, create_employee
 import json
 
 # Here's a class. It inherits from another class.
@@ -116,6 +116,10 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "employees":
             new_item = create_employee(post_body)
+
+        if resource == "customers":
+            new_item = create_customer(post_body)
+
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_item}".encode())
