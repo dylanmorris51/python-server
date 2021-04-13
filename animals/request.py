a3fd9dad6d9622e676ccf7d62ec3ea2850f17d7a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-13 15:04:56 -0500

    typed in a function to fetch animals from the database and convert them to python lists and dictionaries

--- animals/request.py
+++ animals/request.py
@@ -1,3 +1,6 @@
+import sqlite3
+import json
+from models import Animal
 
 # DATA FROM CHAPTER
 
@@ -30,7 +33,44 @@ ANIMALS = [
 
 # Function to return all animals
 def get_all_animals():
-    return ANIMALS
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
+
+        # JUst use these. It's a black box
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the info you want
+        db_cursor.execute("""
+            SELECT
+                a.id,
+                a.name,
+                a.breed,
+                a.status,
+                a.location_id,
+                a.customer_id
+            FROM animal a
+        """)
+
+        # Initialize an empty list to hold all animal representations 
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # the exact order of the parameters defined in the
+            # Animal class above
+            animal = Animal(row['id'], row['name]', row['breed'], row['status'], row['location_id'], row['customer_id']])
+
+            animals.append(animal.__dict__)
+
+        # Use json package to properly serialize list as JSON
+        return json.dumps(animals)
 
 
 # Function with a single parameter
