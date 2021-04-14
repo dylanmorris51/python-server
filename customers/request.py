7ed9e81f95776e366eff2ee9a873c5122001bab0
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-14 10:56:04 -0500

    fixed typo so postman fetch works too

--- customers/request.py
+++ customers/request.py
@@ -34,7 +34,7 @@ def get_all_customers():
                 c.id,
                 c.name,
                 c.address,
-                c.email,
+                c.email
             FROM customer c
         """)
 
@@ -59,7 +59,7 @@ def get_single_customer(id):
                 c.id,
                 c.name,
                 c.address,
-                c.email,
+                c.email
             FROM customer c
         """)
 
