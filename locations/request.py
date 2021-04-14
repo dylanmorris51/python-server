811df32768f5b0cf0155d5a47fe3b7e7c6298c6c
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-14 13:52:03 -0500

    got everything working in ch9

--- locations/request.py
+++ locations/request.py
@@ -50,13 +50,23 @@ def get_all_locations():
         return json.dumps(locations)
 
 def get_single_location(id):
-    requested_location = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    for location in LOCATIONS:
-        if location["id"] == id:
-            requested_location = location
+        db_cursor.execute("""
+            SELECT
+                l.id,
+                l.name,
+                l.address
+            FROM location l
+            WHERE l.id = ?
+        """, (id,))
 
-            return requested_location
+        data = db_cursor.fetchone()
+        location = Locations(data['id'], data['name'], data['address'])
+
+        return json.dumps(location.__dict__)
 
 def create_location(location):
     max_id = LOCATIONS[-1]["id"]
