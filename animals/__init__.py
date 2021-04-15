698d5d349d6a38a577f86d4094839e90f54a2b67
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-15 13:42:24 -0500

    delete animal by id worked

--- animals/__init__.py
+++ animals/__init__.py
@@ -2,3 +2,4 @@ from .request import get_all_animals
 from .request import get_single_animal
 from .request import get_animals_by_location
 from .request import get_animals_by_status
+from .request import delete_animal
