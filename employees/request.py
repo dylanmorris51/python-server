48a486ac0f3106f1b5f1f5c4c15ea53bbd2ca216
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-15 11:31:10 -0500

    got employees by location

--- employees/request.py
+++ employees/request.py
@@ -45,3 +45,27 @@ def get_single_employee(id):
 
         employee = Employee(data['id'], data['name'], data['location_id'])
         return json.dumps(employee.__dict__)
+
+def get_employees_by_location(location_id):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+            SELECT
+                e.id,
+                e.name,
+                e.location_id
+            FROM employee e
+            WHERE e.location_id = ?
+        """, (location_id,))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['location_id'])
+            employees.append(employee.__dict__)
+
+    return json.dumps(employees)
--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, get_animals_by_location
 from locations import get_all_locations, get_single_location
 from customers import get_all_customers, get_single_customer, get_customers_by_email
-from employees import get_all_employees, get_single_employee
+from employees import get_all_employees, get_single_employee, get_employees_by_location
 import json
 
 # Here's a class. It inherits from another class.
@@ -117,6 +117,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             elif key == "location_id" and resource == "animals":
                 response = get_animals_by_location(value)
+
+            elif key == "location_id" and resource == "employees":
+                response = get_employees_by_location(value)
 
         
         # This weird code sends a response back to the client
